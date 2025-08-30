a8d99ad5ff2b776cb6fd608a44f8c5c2e64556c5
fuyuzheju <1713002@qq.com>
2025-08-30 14:38:00 +0800

    add current_app proxy object

--- app/controls.py
+++ app/controls.py
@@ -14,3 +14,30 @@ class GlobalSignal:
             callback()
 
 quit_signal = GlobalSignal()
+
+# code below implements a proxy for global visit of attributes of the app
+# you can import current_app in every corner of the project,
+# to directly refer some attributes of the app object.
+# ---- WARNING ----
+# minimize the reference of current_app, 
+# which increases code coupling, makes dependencies implicit, and decreases maintainability
+# _app_instance = None
+# def register_app(app):
+#     global _app_instance
+#     if _app_instance is not None:
+#         raise RuntimeError("App has been registered.")
+#     _app_instance = app
+
+# def get_app():
+#     if _app_instance is None:
+#         raise RuntimeError("App has not been registered yet.")
+
+# class CurrentAppProxy:
+#     def __getattr__(self, name):
+#         return getattr(get_app(), name)
+
+#     def __setattr__(self, name, value):
+#         return setattr(get_app(), name, value)
+
+# current_app = CurrentAppProxy()
+
--- worktree.py
+++ worktree.py
@@ -5,7 +5,9 @@ def global_exception_hook(exctype, value, tb):
 
 if __name__ == '__main__':
     from app import Application
+    # from app.controls import register_app
     app = Application(sys.argv)
+    # register_app(app)
     sys.excepthook = global_exception_hook
     logger = logging.getLogger()
 
